8ddb92673f8a58085db817dca6f6f999163cfe0b
Georjane <pzgeorjane@gmail.com>
2020-09-01 15:46:16 +0100

    Add view for new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,9 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    # @event = Event.new
+    @event = current_user.events.build
+
   end
 
   # GET /events/1/edit
@@ -22,7 +24,8 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    # @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -68,6 +71,7 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.fetch(:event, {})
+    # params.fetch(:event, {})
+    params.require(:event).permit(:event_name, :event_location, :event_date)
   end
 end
