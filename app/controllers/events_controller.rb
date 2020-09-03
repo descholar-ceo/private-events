9d77ebf729d6937bb36ffab7a28616a27e19dbdc
Georjane <pzgeorjane@gmail.com>
2020-09-03 11:02:55 +0100

    Add scope for users and events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,9 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    # @events = Event.all
+    @past_events = Event.all_past_events
+    @upcoming_events = Event.all_upcoming_events
   end
 
   # GET /events/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :event_creator, class_name: 'Event'
   has_many :invitations, foreign_key: :event_attendee, class_name: 'Invitation'
   has_many :attended_event, through: :invitations, source: :attended_event
+
+  scope :all_past_events, -> {where(["event_date < ?", Date.today])}
+  scope :all_upcoming_events, -> {where(["event_date > ?", Date.today])}
 end
