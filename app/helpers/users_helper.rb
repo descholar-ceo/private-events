788897910d6fd380be2132ffef84c51198d7451f
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-03 15:04:57 +0200

    Fix rubocop lint error

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,5 @@
 module UsersHelper
   def display_events(events, partial_to_render, title_to_display)
-    if events.size.positive?
-      puts title_to_display
-      render partial: partial_to_render, locals: {events: events, title: title_to_display}
-    end
+    render partial: partial_to_render, locals: { events: events, title: title_to_display } if events.size.positive?
   end
 end
