8d6258aad787540b9f81559563830b908c0a0755
Georjane <pzgeorjane@gmail.com>
2020-09-03 12:04:16 +0100

    Add User helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def display_events(events, partial_to_render, title_to_display)
+    if events.size.positive?
+      puts title_to_display
+      render partial: partial_to_render, locals: {events: events, title: title_to_display}
+    end
+  end
 end
