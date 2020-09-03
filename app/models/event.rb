ee8152cd6867993a9051b27d168e255b20b02f25
Georjane <pzgeorjane@gmail.com>
2020-09-03 11:04:55 +0100

    Prefered String literal

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :attended_event
   has_many :event_attendee, through: :invitations, source: :event_attendee
 
-  scope :all_past_events, -> {where(["event_date < ?", Date.today])}
-  scope :all_upcoming_events, -> {where(["event_date > ?", Date.today])}
+  scope :all_past_events, -> { where(['event_date < ?', Date.today]) }
+  scope :all_upcoming_events, -> { where(['event_date > ?', Date.today]) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   has_many :invitations, foreign_key: :event_attendee, class_name: 'Invitation'
   has_many :attended_event, through: :invitations, source: :attended_event
 
-  scope :all_past_events, -> {where(["event_date < ?", Date.today])}
-  scope :all_upcoming_events, -> {where(["event_date > ?", Date.today])}
+  scope :all_past_events, -> { where(['event_date < ?', Date.today]) }
+  scope :all_upcoming_events, -> { where(['event_date > ?', Date.today]) }
 end
