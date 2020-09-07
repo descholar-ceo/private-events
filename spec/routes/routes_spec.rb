f0308e0fbb5a871257d7405dae6dc603e627013c
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-07 19:53:41 +0200

    Test the signup

--- spec/routes/routes_spec.rb
+++ spec/routes/routes_spec.rb
@@ -8,10 +8,19 @@ RSpec.describe 'Homepage', type: :request do
     end
   end
 
-  describe 'GET #login' do
+  describe 'GET #Signin' do
     before { get new_user_session_path }
 
-    it 'returns http success' do
+    it 'returns success' do
+      expect(response).to have_http_status(200)
+    end
+    it { should render_template('new') }
+  end
+
+  describe 'GET #Signup' do
+    before { get new_user_registration_path }
+
+    it 'returns success' do
       expect(response).to have_http_status(200)
     end
     it { should render_template('new') }
